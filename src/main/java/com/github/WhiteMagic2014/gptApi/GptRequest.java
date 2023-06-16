0d5dc0c57d1831d42aca50550608634e45c2c39e
magic chen <418379149@qq.com>
2023-06-16 10:59:42 +0800

    - Optimize:Now, with the use of System.setProperty, you can easily set properties without the need to manually set them each time a request is created.

--- src/main/java/com/github/WhiteMagic2014/gptApi/GptRequest.java
+++ src/main/java/com/github/WhiteMagic2014/gptApi/GptRequest.java
@@ -12,7 +12,8 @@ import com.github.WhiteMagic2014.util.GptHttpUtil;
 public abstract class GptRequest {
 
     /**
-     * default server
+     * Optional
+     * Proxy server
      */
     protected String server = System.getProperty("OPENAI_API_SERVER", "https://api.openai.com");
 
@@ -20,13 +21,13 @@ public abstract class GptRequest {
      * Required
      * OpenAI key, Use api need your key
      */
-    protected String key;
+    protected String key = System.getProperty("OPENAI_API_KEY");
 
     /**
      * Optional
      * OpenAI-Organization, For users who belong to multiple organizations, you can pass a header to specify which organization is used for an API request. Usage from these API requests will count against the specified organization's subscription quota.
      */
-    protected String org;
+    protected String org = System.getProperty("OPENAI_API_ORG");
 
     protected GptHttpUtil gptHttpUtil = new DefaultGptHttpUtil();
 
@@ -37,15 +38,8 @@ public abstract class GptRequest {
         if (gptHttpUtil == null) {
             throw new RuntimeException("missing GptHttpUtil");
         }
-        if (key == null || "".equals(key.trim())) {
-            String keySetting = System.getProperty("OPENAI_API_KEY");
-            if (keySetting == null || "".equals(keySetting.trim())) {
-                throw new RuntimeException("missing GptKey");
-            }
-            key = keySetting;
-        }
-        if (org == null) {
-            org = System.getProperty("OPENAI_API_ORG");
+        if (key == null) {
+            throw new RuntimeException("missing GptKey");
         }
         String resp = sendHook();
         if (resp == null) {
