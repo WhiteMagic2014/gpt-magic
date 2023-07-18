239e3a03eab32a20d3ac0b29464d9b1ac95a9e64
magic chen <418379149@qq.com>
2023-07-18 16:14:00 +0800

    - Update: update models in GptModel.class - Deprecated: CreateCompletionRequest will be Deprecated

--- src/main/java/com/github/WhiteMagic2014/gptApi/GptModel.java
+++ src/main/java/com/github/WhiteMagic2014/gptApi/GptModel.java
@@ -22,7 +22,7 @@ public class GptModel {
      * Snapshot of gpt-4 from June 13th 2023 with function calling data. Unlike gpt-4, this model will not receive updates, and will be deprecated 3 months after a new version is released.
      * 8192 max tokens
      */
-    public final static String gpt_4_0314 = "gpt-4-0613";
+    public final static String gpt_4_0613 = "gpt-4-0613";
 
     /**
      * Same capabilities as the base gpt-4 mode but with 4x the context length. Will be updated with our latest model iteration.
@@ -34,7 +34,7 @@ public class GptModel {
      * Snapshot of gpt-4-32 from June 13th 2023. Unlike gpt-4-32k, this model will not receive updates, and will be deprecated 3 months after a new version is released.
      * 32768 max tokens
      */
-    public final static String gpt_4_32k_0314 = "gpt-4-32k-0613";
+    public final static String gpt_4_32k_0613 = "gpt-4-32k-0613";
 
     /**
      * Most capable GPT-3.5 model and optimized for chat at 1/10th the cost of text-davinci-003. Will be updated with our latest model iteration 2 weeks after it is released.
